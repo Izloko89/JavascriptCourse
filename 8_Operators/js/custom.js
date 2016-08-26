7621628de224d550be3a8cf43e819c2e5e44c6ba
Vic <viciz.1089@gmail.com>
2016-08-25 19:54:43 -0500

    Operators finish

--- 8_Operators/js/custom.js
+++ 8_Operators/js/custom.js
@@ -4,3 +4,12 @@ function assOp () {
 	x += 5;
 	document.getElementById("demo1").innerHTML = x;
 }
+
+// Strings and Numbers
+function strNum () {
+	var x = 5 + 5;
+	var y = "5" + 5;
+	var z = "Hello" + 5;
+	document.getElementById("demo2").innerHTML =
+	x + "<br>" + y + "<br>" + z;
+}
